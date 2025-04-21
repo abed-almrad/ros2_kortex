1bf24c45e40c0e4176393c0821a5d17c378df266
Abed <robotogeddon@gmail.com>
2025-04-21 14:32:34 -0400

    Added a modified launch file to be started initially to command the real-life Gen3 7DoF

---
+++ kortex_bringup/launch/gen3_rl_control.launch.py
@@ -0,0 +1,337 @@
+# Copyright (c) 2021 PickNik, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Marq Rasmussen, Denis Stogl
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    PythonExpression,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    # Initialize Arguments
+    robot_ip = LaunchConfiguration("robot_ip")
+    # General arguments
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    robot_name = LaunchConfiguration("robot_name")
+    prefix = LaunchConfiguration("prefix")
+    gripper = LaunchConfiguration("gripper")
+    gripper_max_velocity = LaunchConfiguration("gripper_max_velocity")
+    gripper_max_force = LaunchConfiguration("gripper_max_force")
+    robot_traj_controller = LaunchConfiguration("robot_controller")
+    robot_pos_controller = LaunchConfiguration("robot_pos_controller")
+    robot_hand_controller = LaunchConfiguration("robot_hand_controller")
+    fault_controller = LaunchConfiguration("fault_controller")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    use_internal_bus_gripper_comm = LaunchConfiguration("use_internal_bus_gripper_comm")
+    gripper_joint_name = LaunchConfiguration("gripper_joint_name")
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "robots", description_file]
+            ),
+            " ",
+            "robot_ip:=",
+            robot_ip,
+            " ",
+            "name:=",
+            robot_name,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "gripper:=",
+            gripper,
+            " ",
+            "use_internal_bus_gripper_comm:=",
+            use_internal_bus_gripper_comm,
+            " ",
+            "gripper_max_velocity:=",
+            gripper_max_velocity,
+            " ",
+            "gripper_max_force:=",
+            gripper_max_force,
+            " ",
+            "gripper_joint_name:=",
+            gripper_joint_name,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "gen3_arm/config",
+            controllers_file,
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
+        output="both",
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        condition=IfCondition(launch_rviz),
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        ),
+        condition=IfCondition(launch_rviz),
+    )
+
+    robot_traj_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_traj_controller, "-c", "/controller_manager"],
+    )
+
+    robot_pos_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_pos_controller, "--inactive", "-c", "/controller_manager"],
+    )
+
+    robot_hand_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_hand_controller, "-c", "/controller_manager"],
+    )
+
+    # only start the fault controller if we are using hardware
+    fault_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[fault_controller, "-c", "/controller_manager"],
+        condition=IfCondition(use_internal_bus_gripper_comm),
+    )
+
+    nodes_to_start = [
+        control_node,
+        robot_state_publisher_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        robot_traj_controller_spawner,
+        robot_pos_controller_spawner,
+        robot_hand_controller_spawner,
+        fault_controller_spawner,
+    ]
+
+    return nodes_to_start
+
+def generate_launch_description():
+    declared_arguments = []
+    # Robot specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_ip", description="IP address by which the robot can be reached."
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "username", description="Robot session username.", default_value="admin"
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "password", description="Robot session password.", default_value="admin"
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "port", description="Robot port for tcp connection.", default_value="10000"
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "port_realtime",
+            description="Robot port for udp realtime control.",
+            default_value="10001",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "session_inactivity_timeout_ms",
+            description="Robot session inactivity timeout in milliseconds.",
+            default_value="60000",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "connection_inactivity_timeout_ms",
+            description="Robot connection inactivity timeout in milliseconds.",
+            default_value="2000",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ros2_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="kortex_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="gen3.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_name",
+            default_value="arm",
+            description="Name of the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper",
+            default_value="robotiq_2f_85",
+            description="Name of the gripper attached to the arm",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_pos_controller",
+            default_value="twist_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_hand_controller",
+            default_value="robotiq_gripper_controller",
+            description="Robot hand controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fault_controller",
+            default_value="fault_controller",
+            description="Name of the 'fault controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_internal_bus_gripper_comm",
+            default_value="true",
+            description="Use internal bus for gripper communication?",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_velocity",
+            default_value="100.0",
+            description="Max velocity for gripper commands",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_max_force",
+            default_value="100.0",
+            description="Max force for gripper commands",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper_joint_name",
+            default_value="robotiq_85_left_knuckle_joint",
+            description="Max force for gripper commands",
+        )
+    )
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
